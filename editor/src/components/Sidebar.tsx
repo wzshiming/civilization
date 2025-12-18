e923115ef5fec4197ca7627c48f7ad367527fecf
Shiming Zhang <wzshiming@hotmail.com>
2025-12-18 18:04:53 +0800

    Clean up

--- editor/src/components/Sidebar.tsx
+++ editor/src/components/Sidebar.tsx
@@ -148,26 +148,6 @@ const Sidebar: React.FC<SidebarProps> = ({
       </div>
 
       <div className={styles.panel}>
-        <h2>TERRAIN TYPES</h2>
-        <div className={styles.terrainGrid}>
-          {map?.terrainTypes.map((terrain) => (
-            <button
-              key={terrain.terrainTypeID}
-              onClick={() => onSelectTerrain(terrain.terrainTypeID)}
-              className={`${styles.terrainButton} ${
-                selectedTerrain === terrain.terrainTypeID ? styles.selected : ''
-              }`}
-              style={{
-                backgroundColor: terrain.color || "#888888",
-              }}
-            >
-              {terrain.name}
-            </button>
-          ))}
-        </div>
-      </div>
-
-      <div className={styles.panel}>
         <h2>HISTORY</h2>
         <div className={styles.historyButtons}>
           <button
